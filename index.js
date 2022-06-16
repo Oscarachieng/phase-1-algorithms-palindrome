8fda4821ba40ce0100ed6fbc32f63b069b9a3f7a
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-16 15:05:12 +0300

    commit tree 9e1cdbac506b9a8f3845b3f854aa1674fe143cb2

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const testWord= reverseHandler(word);
+  if (word === testWord) {
+    return true;
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  1. declare the function that takes a string variable
+  2. let splittedString=String.Split()
+    let ReversedString= splittedString.Reverse()
+    let finalReversedString = ReversedString.Join()
+  3. if string === finalReversedString
+  4. Return true
+
+  5.Else Retur false.
 */
 
+const reverseHandler = (word) => {
+  return word.split("").reverse().join("")
+}
+
+console.log(isPalindrome("oscar"))
 /*
   Add written explanation of your solution here
 */
